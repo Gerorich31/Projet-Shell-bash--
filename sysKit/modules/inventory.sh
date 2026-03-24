92dbcbf2eac24f0ad580320a2d7268b4ae7bdf04
BOKOPE <gerorich40@gmail.com>
2026-03-24 20:08:55 +0100

    Modification du script inventory

--- sysKit/modules/inventory.sh
+++ sysKit/modules/inventory.sh
@@ -3,30 +3,33 @@ echo""
 echo "===== Informations système ====="
 
 echo""
-
+#Systeme d'exploitation
 echo "Système d'exploitation :"
 uname -o
 
-
+#Version du noyau
 
 echo "Version du noyau :"
 uname -r
 
-
+#Infomation sur le Processeur 
 echo "Processeur :"
 lscpu | grep "Model name"
 
 
-
+#Information sur la Ram
 echo "Mémoire RAM :"
 free -h
 
 
-
+#Information sur le disque
 echo "Espace disque :"
 df -h
 
 
-
+#Information sur le disque
 echo "Adresse IP :"
 hostname -I
+echo ""
+echo ""
+echo "Description des caracteristiques d'un ordinateur"
